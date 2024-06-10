25ec4d4252a91158100af8577b17db81a2f16bda
phucne123111 <134253765+phucne123111@users.noreply.github.com>
2024-06-10 18:58:40 +0700

    fixxxx
    hihi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Weather from './Login';
+import Weather from './Weather';
 import WeatherMap from './Map';
 import WeatherChart from './Chart';
 
